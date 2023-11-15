a8bbeca61ace673e74e4fc5550b26f8d5125284b
Gokul Murugesan <gokulmurugesan@192.168.1.8>
2023-11-15 19:58:01 +0530

    upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+
+import React from 'react';
+import UserTable from './UserTable';
+
+function App() {
+  return (
+    <div>
+      <UserTable />
+    </div>
+  );
+}
+
+export default App;
